ce14d46113efdde0e9b90c2ebcce7b9db3c13da5
hesicong <derekhe@april1985.com>
2016-02-06 13:11:12 +0800

    websocket正常工作

--- control/app/main.js
+++ control/app/main.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
+import SockJS from '../node_modules/sockjs-client'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+var sock = new SockJS('http://localhost:9999/echo');
+sock.onopen = function() {
+    console.log('open');
+};
+sock.onmessage = function(e) {
+    console.log('message', e.data);
+};
+sock.onclose = function() {
+    console.log('close');
+};
--- control/server.js
+++ control/server.js
@@ -45,3 +45,17 @@ app.listen(port, '0.0.0.0', function onStart(err) {
   }
   console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
+
+var http = require('http');
+var sockjs = require('sockjs');
+var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
+echo.on('connection', function(conn) {
+  conn.on('data', function(message) {
+    conn.write(message);
+  });
+  conn.on('close', function() {});
+});
+
+var server = http.createServer();
+echo.installHandlers(server, {prefix:'/echo'});
+server.listen(9999, '0.0.0.0');
