dc9c78ca8b5a3470aa2d4f8d5966459cc8cea138
hesicong <derekhe@april1985.com>
2016-02-08 22:04:48 +0800

    增加touch事件处理

--- control/public/js/app.js
+++ control/public/js/app.js
@@ -88,4 +88,22 @@ app.controller("carController", function ($scope, $window) {
 
         lastKeycode = null;
     };
-});
+}).directive('myTouchstart', [function() {
+    return function(scope, element, attr) {
+
+        element.on('touchstart', function(event) {
+            scope.$apply(function() {
+                scope.$eval(attr.myTouchstart);
+            });
+        });
+    };
+}]).directive('myTouchend', [function() {
+    return function(scope, element, attr) {
+
+        element.on('touchend', function(event) {
+            scope.$apply(function() {
+                scope.$eval(attr.myTouchend);
+            });
+        });
+    };
+}]);
