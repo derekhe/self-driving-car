a22b2650ab0a24b42440eb934fe49fce9a27ea48
hesicong <derekhe@april1985.com>
2016-04-26 21:26:55 +0800

    Update for control lego car

--- arduino/lego/lego.ino
+++ arduino/lego/lego.ino
@@ -4,21 +4,41 @@
 
 char DRIVE_PIN1 = 12;
 char DRIVE_PIN2 = 11;
+char DRIVE_ENABLE_PIN = 3;
 
 Servo myservo;
 
 SerialCommand sCmd;
 
+int readArgument(int defaultValue = 255){
+  int argument = 0;
+  char *arg;
+
+  arg = sCmd.next();  
+  if (arg != NULL) {
+    return atoi(arg);
+  }
+  else{
+    return defaultValue;
+  }
+}
+
 void forward() {
+  int speed = readArgument(255);  
   digitalWrite(DRIVE_PIN1, 1);
   digitalWrite(DRIVE_PIN2, 0);
+  analogWrite(DRIVE_ENABLE_PIN, speed);
   Serial.println("forward");
+  Serial.println(speed);
 }
 
 void backward() {
+  int speed = readArgument(255);
   digitalWrite(DRIVE_PIN1, 0);
   digitalWrite(DRIVE_PIN2, 1);
+  analogWrite(DRIVE_ENABLE_PIN, speed);
   Serial.println("backward");
+  Serial.println(speed);
 }
 
 void pause() {
@@ -28,19 +48,10 @@ void pause() {
 
 }
 
-void left() {
-  myservo.write(0);
-  Serial.println("left");
-}
-
-void right() {
-  myservo.write(180);
-  Serial.println("right");
-}
-
-void straight() {
-  myservo.write(90);
-  Serial.println("straight");
+void steer() {
+  int angle = readArgument(90);
+  myservo.write(angle);
+  Serial.println("steer");
 }
 
 void unrecognized(const char *command) {
@@ -51,6 +62,7 @@ void setup() {
   Serial.begin(115200);
   pinMode(DRIVE_PIN1, OUTPUT);
   pinMode(DRIVE_PIN2, OUTPUT);
+  pinMode(DRIVE_ENABLE_PIN, OUTPUT);
 
   myservo.attach(9);
   myservo.write(90);
@@ -59,10 +71,8 @@ void setup() {
 
   sCmd.addCommand("F", forward);
   sCmd.addCommand("B", backward);
-  sCmd.addCommand("L", left);
-  sCmd.addCommand("R", right);
+  sCmd.addCommand("S", steer);
   sCmd.addCommand("P", pause);
-  sCmd.addCommand("S", straight);
   sCmd.setDefaultHandler(unrecognized);
 }
 
